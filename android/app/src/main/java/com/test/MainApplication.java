4e27fa71b09c67b36c3bc70478bc32e9e575c944
developer <developer@developers-MacBook-Air.local>
2018-11-13 17:28:12 +0200

    IOS Done With sql download and extract and read

--- android/app/src/main/java/com/test/MainApplication.java
+++ android/app/src/main/java/com/test/MainApplication.java
@@ -3,6 +3,8 @@ package com.test;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnziparchive.RNZipArchivePackage;
+import com.RNFetchBlob.RNFetchBlobPackage;
 import cl.json.RNSharePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
         new SQLitePluginPackage(), 
           new MainReactPackage(),
+            new RNZipArchivePackage(),
+            new RNFetchBlobPackage(),
             new RNSharePackage()
       );
     }
--- src/screens/rearrange-list/List.js
+++ src/screens/rearrange-list/List.js
@@ -4,7 +4,10 @@ import BlueView from "../../components/blue-view/BlueView";
 import { Spinner } from "native-base";
 import { Services } from "../../models/Services";
 import api from "../../api/api-class/ApiController";
+import { unzip } from "react-native-zip-archive";
+import RNFetchBlob from "rn-fetch-blob";
 var SQLite = require("react-native-sqlite-storage");
+
 class List extends React.Component {
   static navigationOptions = {
     title: "LIST".toUpperCase(),
@@ -28,53 +31,81 @@ class List extends React.Component {
     this.handleGetToken();
   }
   componentDidMount() {
-    let errorCB = err => {
-      console.log("SQL Error: " + err);
-    };
-
-    let successCB = () => {
-      console.log("SQL executed fine");
-    };
-
-    let openCB = () => {
-      console.log("Database OPENED");
-    };
-
-    var db = SQLite.openDatabase(
-     { name:"chinook.db",readOnly: true,createFromLocation :1},
-      openCB,
-      errorCB
-    );
-    // db.transaction(tx=>{
-    //     tx.executeSql("CREATE TABLE Employees (Id Int, name Varchar);")
-    // })
-    // db.transaction(tx => {
-    //   tx.executeSql("INSERT INTO Employees (name) VALUES ('Sandy')", [], (tx, results) => {
-    //   });
-    // });
-    console.log(db)
-    db.transaction(tx => {
-        console.log(tx)
-      tx.executeSql("SELECT * FROM  employees;",[], (tx, results) => {
-        console.log("Query completed");
-        console.log(results)
-        // // Get rows with Web SQL Database spec compliance.
-
-        var len = results.rows.length;
-        for (let i = 0; i < len; i++) {
-          let row = results.rows.item(i);
-          console.log(`Employee name: ${row.FirstName}`);
-        }
-
-        // // Alternatively, you can use the non-standard raw method.
-
-        // let rows = results.rows.raw(); // shallow copy of rows Array
-
-        // rows.map(row =>
-        //   console.log(`Employee name: ${row.name}, Dept Name: ${row.deptName}`)
-        // );
+  RNFetchBlob.fs
+      .ls(RNFetchBlob.fs.dirs.DocumentDir+"/unzipped")
+      // files will an array contains filenames
+      .then(files => {
+        console.log(files);
       });
-    });
+
+    RNFetchBlob.config({
+      // add this option that makes response data to be stored as a file,
+      // this is much more performant.
+      fileCache: true,
+      path: RNFetchBlob.fs.dirs.DocumentDir
+    })
+      .fetch(
+        "GET",
+        "http://www.sqlitetutorial.net/wp-content/uploads/2018/03/chinook.zip",
+        {
+          //some headers ..
+        }
+      )
+      .then(res => {
+        const sourcePath = `${RNFetchBlob.fs.dirs.DocumentDir}/chinook.zip`;
+        const targetPath = `${RNFetchBlob.fs.dirs.DocumentDir}/unzipped`;
+        unzip(sourcePath, targetPath)
+          .then(path => {
+            console.log(`unzip completed at ${RNFetchBlob.fs.dirs.DocumentDir}`);
+            let errorCB = err => {
+              console.log("SQL Error: " + err);
+            };
+            let successCB = () => {
+              console.log("SQL executed fine");
+            };
+            let openCB = () => {
+              console.log("Database OPENED");
+            };
+            console.log("HEEEEEEY",`${RNFetchBlob.fs.dirs.DocumentDir}`)
+            var db = SQLite.openDatabase(
+             { name:"chinook.db",createFromLocation :"/unzipped/chinook.db",readOnly:true,location:"Library"},
+              openCB,
+              errorCB
+            );
+            // db.transaction(tx=>{
+            //     tx.executeSql("CREATE TABLE Employees (Id Int, name Varchar);")
+            // })
+            // db.transaction(tx => {
+            //   tx.executeSql("INSERT INTO Employees (name) VALUES ('Sandy')", [], (tx, results) => {
+            //   });
+            // });
+            console.log(db)
+            db.transaction(tx => {
+                console.log("tx",tx)
+              tx.executeSql("SELECT * FROM  employees;",[], (tx, results) => {
+                console.log("Query completed");
+                console.log(results)
+                // // Get rows with Web SQL Database spec compliance.
+                var len = results.rows.length;
+                for (let i = 0; i < len; i++) {
+                  let row = results.rows.item(i);
+                  console.log(`Employee name: ${row.FirstName}`);
+                }
+                // // Alternatively, you can use the non-standard raw method.
+                // let rows = results.rows.raw(); // shallow copy of rows Array
+                // rows.map(row =>
+                //   console.log(`Employee name: ${row.name}, Dept Name: ${row.deptName}`)
+                // );
+              });
+            });
+          })
+          .catch(error => {
+            console.log(error);
+          });
+        console.log(res);
+        // the temp file path
+        console.log("The file saved to ", res.path());
+      });
   }
   getTokenCallBack = async (response, err) => {
     if (response.data.token) {
@@ -126,5 +157,82 @@ class List extends React.Component {
     );
   }
 }
+// ios{
+  // RNFetchBlob.fs
+    //   .ls(RNFetchBlob.fs.dirs.DocumentDir+"/unzipped")
+    //   // files will an array contains filenames
+    //   .then(files => {
+    //     console.log(files);
+    //   });
+
+//     RNFetchBlob.config({
+//       // add this option that makes response data to be stored as a file,
+//       // this is much more performant.
+//       fileCache: true,
+//       path: RNFetchBlob.fs.dirs.DocumentDir
+//     })
+//       .fetch(
+//         "GET",
+//         "http://www.sqlitetutorial.net/wp-content/uploads/2018/03/chinook.zip",
+//         {
+//           //some headers ..
+//         }
+//       )
+//       .then(res => {
+//         const sourcePath = `${RNFetchBlob.fs.dirs.DocumentDir}/chinook.zip`;
+//         const targetPath = `${RNFetchBlob.fs.dirs.DocumentDir}/unzipped`;
+//         unzip(sourcePath, targetPath)
+//           .then(path => {
+//             console.log(`unzip completed at ${RNFetchBlob.fs.dirs.DocumentDir}`);
+//             let errorCB = err => {
+//               console.log("SQL Error: " + err);
+//             };
+//             let successCB = () => {
+//               console.log("SQL executed fine");
+//             };
+//             let openCB = () => {
+//               console.log("Database OPENED");
+//             };
+//             console.log("HEEEEEEY",`${RNFetchBlob.fs.dirs.DocumentDir}`)
+//             var db = SQLite.openDatabase(
+//              { name:"chinook.db",createFromLocation :"/unzipped/chinook.db",readOnly:true,location:"Library"},
+//               openCB,
+//               errorCB
+//             );
+//             // db.transaction(tx=>{
+//             //     tx.executeSql("CREATE TABLE Employees (Id Int, name Varchar);")
+//             // })
+//             // db.transaction(tx => {
+//             //   tx.executeSql("INSERT INTO Employees (name) VALUES ('Sandy')", [], (tx, results) => {
+//             //   });
+//             // });
+//             console.log(db)
+//             db.transaction(tx => {
+//                 console.log("tx",tx)
+//               tx.executeSql("SELECT * FROM  employees;",[], (tx, results) => {
+//                 console.log("Query completed");
+//                 console.log(results)
+//                 // // Get rows with Web SQL Database spec compliance.
+//                 var len = results.rows.length;
+//                 for (let i = 0; i < len; i++) {
+//                   let row = results.rows.item(i);
+//                   console.log(`Employee name: ${row.FirstName}`);
+//                 }
+//                 // // Alternatively, you can use the non-standard raw method.
+//                 // let rows = results.rows.raw(); // shallow copy of rows Array
+//                 // rows.map(row =>
+//                 //   console.log(`Employee name: ${row.name}, Dept Name: ${row.deptName}`)
+//                 // );
+//               });
+//             });
+//           })
+//           .catch(error => {
+//             console.log(error);
+//           });
+//         console.log(res);
+//         // the temp file path
+//         console.log("The file saved to ", res.path());
+//       });
+// }
 
 export default List;
