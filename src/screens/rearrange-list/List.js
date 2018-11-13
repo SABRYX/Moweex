1133ceb934a7241e4f175dbfc48dc205fef1bdbb
developer <developer@developers-MacBook-Air.local>
2018-11-13 18:53:22 +0200

    working for both

--- src/screens/rearrange-list/List.js
+++ src/screens/rearrange-list/List.js
@@ -31,12 +31,52 @@ class List extends React.Component {
     this.handleGetToken();
   }
   componentDidMount() {
-  RNFetchBlob.fs
-      .ls(RNFetchBlob.fs.dirs.DocumentDir+"/unzipped")
+    RNFetchBlob.fs
+      .ls(RNFetchBlob.fs.dirs.DocumentDir + "/unzipped")
       // files will an array contains filenames
       .then(files => {
         console.log(files);
       });
+    let errorCB = err => {
+      console.log("SQL Error: " + err);
+    };
+    let successCB = () => {
+      console.log("SQL executed fine");
+    };
+    let openCB = () => {
+      console.log("Database OPENED");
+    };
+    var db = SQLite.openDatabase(
+      { name: "chinook.db", createFromLocation:"/unzipped/chinook.db", readOnly: true },
+      openCB,
+      errorCB
+    );
+    console.log(db);
+    db.transaction(tx => {
+      console.log("tx", tx);
+      tx.executeSql(
+        "SELECT * FROM  employees;",
+        [],
+        (tx, results) => {
+          console.log("Query completed");
+          console.log(results);
+          // // Get rows with Web SQL Database spec compliance.
+          var len = results.rows.length;
+          for (let i = 0; i < len; i++) {
+            let row = results.rows.item(i);
+            console.log(`Employee name: ${row.FirstName}`);
+          }
+          // // Alternatively, you can use the non-standard raw method.
+          // let rows = results.rows.raw(); // shallow copy of rows Array
+          // rows.map(row =>
+          //   console.log(`Employee name: ${row.name}, Dept Name: ${row.deptName}`)
+          // );
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    });
 
     RNFetchBlob.config({
       // add this option that makes response data to be stored as a file,
@@ -56,48 +96,11 @@ class List extends React.Component {
         const targetPath = `${RNFetchBlob.fs.dirs.DocumentDir}/unzipped`;
         unzip(sourcePath, targetPath)
           .then(path => {
-            console.log(`unzip completed at ${RNFetchBlob.fs.dirs.DocumentDir}`);
-            let errorCB = err => {
-              console.log("SQL Error: " + err);
-            };
-            let successCB = () => {
-              console.log("SQL executed fine");
-            };
-            let openCB = () => {
-              console.log("Database OPENED");
-            };
-            console.log("HEEEEEEY",`${RNFetchBlob.fs.dirs.DocumentDir}`)
-            var db = SQLite.openDatabase(
-             { name:"chinook.db",createFromLocation :"/unzipped/chinook.db",readOnly:true,location:"Library"},
-              openCB,
-              errorCB
+            console.log(
+              `unzip completed at ${RNFetchBlob.fs.dirs.DocumentDir}`
             );
-            // db.transaction(tx=>{
-            //     tx.executeSql("CREATE TABLE Employees (Id Int, name Varchar);")
-            // })
-            // db.transaction(tx => {
-            //   tx.executeSql("INSERT INTO Employees (name) VALUES ('Sandy')", [], (tx, results) => {
-            //   });
-            // });
-            console.log(db)
-            db.transaction(tx => {
-                console.log("tx",tx)
-              tx.executeSql("SELECT * FROM  employees;",[], (tx, results) => {
-                console.log("Query completed");
-                console.log(results)
-                // // Get rows with Web SQL Database spec compliance.
-                var len = results.rows.length;
-                for (let i = 0; i < len; i++) {
-                  let row = results.rows.item(i);
-                  console.log(`Employee name: ${row.FirstName}`);
-                }
-                // // Alternatively, you can use the non-standard raw method.
-                // let rows = results.rows.raw(); // shallow copy of rows Array
-                // rows.map(row =>
-                //   console.log(`Employee name: ${row.name}, Dept Name: ${row.deptName}`)
-                // );
-              });
-            });
+
+            console.log("HEEEEEEY", `${RNFetchBlob.fs.dirs.DocumentDir}`);
           })
           .catch(error => {
             console.log(error);
@@ -158,12 +161,12 @@ class List extends React.Component {
   }
 }
 // ios{
-  // RNFetchBlob.fs
-    //   .ls(RNFetchBlob.fs.dirs.DocumentDir+"/unzipped")
-    //   // files will an array contains filenames
-    //   .then(files => {
-    //     console.log(files);
-    //   });
+// RNFetchBlob.fs
+//   .ls(RNFetchBlob.fs.dirs.DocumentDir+"/unzipped")
+//   // files will an array contains filenames
+//   .then(files => {
+//     console.log(files);
+//   });
 
 //     RNFetchBlob.config({
 //       // add this option that makes response data to be stored as a file,
